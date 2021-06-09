7eab82d116361410077019adaaa5d6f9e346151a
Paulo Marques <pauloedums@gmail.com>
2021-06-09 20:05:13 -0300

    first commit

---
+++ src/app/04-forms/todo-form.component.ts
@@ -0,0 +1,14 @@
+
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+
+export class TodoFormComponent {
+  form: FormGroup;
+
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      name: ['', Validators.required],
+      lastname: ['last'],
+      email: [''],
+    });
+  }
+}
